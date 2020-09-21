0d5c390b859325870f3afb195c765a426e89acaf
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-21 05:24:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,10 +26,10 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].collect do |holiday, supplies|
+  winter_supplies = holiday_hash[:winter].each do |holiday, supplies|
     supplies
   end
-  
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
